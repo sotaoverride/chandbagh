a5c4aac460c90cc7d76411b74faf75c339419675
Aatir Manzur <aatrapps@gmail.com>
2024-01-04 17:40:23 -0500

    thread for fw response and request

--- test/spi_messaging_emulator.c
+++ test/spi_messaging_emulator.c
@@ -11,57 +11,64 @@ static char buff_master_rx[3];
 static char buff_master_tx[3];
 
 
-static void * slave_sockpair_read(void *pfd, int bytes) {
+static void * slave_sockpair_read(void *pfd, int bytes, int start) {
 	int fd = *((int *)pfd);
 	long index;
-	for (index=1; index < bytes; index++) {
-		read(fd, buff_slave_rx[index-1], 1);
+	for (index=0; index < bytes; index++) {
+		read(fd, buff_slave_rx[index+start], 1);
 	}
 	return NULL;
 }
 
-static void * master_sockpair_read(void *pfd, int bytes) {
+static void * master_sockpair_read(void *pfd, int bytes, int start) {
 	int fd = *((int *)pfd);
 	long index;
-	for (index =1; index < bytes; index++){
+	for (index =0; index < bytes; index++){
 
-		read(fd, buff_master_rx[index-1], 1);
+		read(fd, buff_master_rx[index+start], 1);
 	}
 	return NULL;
 }
 
-static void * slave_sockpair_write(void *pfd, int bytes) {
+static void * slave_sockpair_write(void *pfd, int bytes, int start) {
 	int fd = *((int *)pfd);
 	long index;
-	for (index=1; index < bytes; index++) {
-		write(fd, buff_slave_tx[index], 1);
+	for (index=0; index < bytes; index++) {
+		write(fd, buff_slave_tx[index+1], 1);
 	}
 	return NULL;
 }
 
-static void * master_sockpair_write(void *pfd, int bytes) {
+static void * master_sockpair_write(void *pfd, int bytes, int start) {
 	int fd = *((int *)pfd);
 	long index;
-	for (index=1; index < bytes; index++) {
-		write(fd, buff_master_tx[index], 1);
+	for (index=0; index < bytes; index++) {
+		write(fd, buff_master_tx[index+start], 1);
 	}
 	return NULL;
 }
-
+static request_fw(void *pfd) {
+	master_sockpair_write(pfd, 1, 0);
+	master_sockpair_read(pfd,1,0);
+	master_sockpair_write(pfd, 2, 1);
+	master_sockpair_read(pfd, 2, 1)
+}
+static response_fw(void *pfd) {
+	slave_sockpair_write(pfd, 1, 0);
+	slave_sockpair_read(pfd,1,0);
+	slave_sockpair_write(pfd,2,1);
+	slave_sockpair_read(pfd, 2, 1);
+	
 static void _spi_messaging_test() {
 	int fd[2];
-	pthread_t master_read_thr;
-	pthread_t slave_read_thr;
-	pthread_t master_write_thr;
-	pthread_t slave_write_thr;
+	pthread_t master_thr;
+	pthread_t slave_thr;
 
 	printf("socketpair msg passing for messages\n");
 
 	socketpair(AF_UNIX, SOCK_STREAM, 0, fd);
-	pthread_create(&master_read_thr, NULL, master_sockpair_read, (void *)(&(fd[0])));
-	pthread_create(&slave_read_thr, NULL, slave_sockpair_read, (void *)(&(fd[1])));
-	pthread_create(&master_write_thr, NULL, master_sockpair_write, (void *)(&(fd[0])));
-	pthread_create(&slave_write_thr, NULL, slave_sockpair_write, (void *)(&(fd[1])));
+	pthread_create(&master_thr, NULL, request_fw, (void *)(&(fd[0])));
+	pthread_create(&slave_thr, NULL, response_fw, (void *)(&(fd[1])));
 	close(fd[0]);
 	close(fd[1]);
 }
