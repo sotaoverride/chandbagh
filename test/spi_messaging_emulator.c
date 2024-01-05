6c00f5a08bc898435063144877899f9188f63f1b
Aatir Manzur <aatrapps@gmail.com>
2024-01-04 21:28:19 -0500

    prints added

--- test/spi_messaging_emulator.c
+++ test/spi_messaging_emulator.c
@@ -9,49 +9,50 @@
 int main(void);
 
 
-static char buff_slave_rx[3] = {};
+static char buff_slave_rx[3] = {0xaa, 0xbb, 0xbb};
 static char buff_slave_tx[3] = {0xca, 0xd5, 0xd5};
-static char buff_master_rx[3];
+static char buff_master_rx[3] =  {0xdd, 0xee, 0xcc};
 static char buff_master_tx[3] = {0x5a, 0xad, 0xfc};
 
 
 static void * slave_sockpair_read(void *pfd, int bytes, int start) {
 	int fd = *((int *)pfd);
-	read(fd, buff_slave_rx[index+start], bytes);
+	read(fd, buff_slave_rx + start, bytes);
 	return NULL;
 }
 
 static void * master_sockpair_read(void *pfd, int bytes, int start) {
 	int fd = *((int *)pfd);
-	read(fd, buff_master_rx[index+start], bytes);
+	read(fd, buff_master_rx + start, bytes);
 	return NULL;
 }
 
 static void * slave_sockpair_write(void *pfd, int bytes, int start) {
 	int fd = *((int *)pfd);
-	long index;
-	write(fd, buff_slave_tx[index+start], bytes);
+	write(fd, buff_slave_tx + start, bytes);
 	return NULL;
 }
 
 static void * master_sockpair_write(void *pfd, int bytes, int start) {
 	int fd = *((int *)pfd);
-	long index;
-	write(fd, buff_master_tx[index+start], bytes);
+	write(fd, buff_master_tx + start, bytes);
 	return NULL;
 }
 static request_fw(void *pfd) {
 	master_sockpair_write(pfd, 1, 0);
 	master_sockpair_read(pfd,1,0);
+	printf("0x%x", buff_master_rx[0]);
 	master_sockpair_write(pfd, 2, 1);
 	master_sockpair_read(pfd, 2, 1);
+	printf("0x%x", buff_master_tx[0]);
 }
 static response_fw(void *pfd) {
 	slave_sockpair_write(pfd, 1, 0);
 	slave_sockpair_read(pfd,1,0);
+	printf("0x%x", buff_slave_rx[0]);
 	slave_sockpair_write(pfd,2,1);
 	slave_sockpair_read(pfd, 2, 1);
-	
+	printf("0x%x", buff_slave_tx[0]);
 }
 static void _spi_messaging_test() {
 	int fd[2];
@@ -68,6 +69,7 @@ static void _spi_messaging_test() {
 }
 
 int main(void){
+	printf("0x%x", buff_master_tx[0]);
 	_spi_messaging_test();
 	return 0;
 }
