8b6e1d3e6903b5dc624b8084e71fed06e96857cc
Aatir Manzur <aatrapps@gmail.com>
2024-01-23 15:21:32 -0500

    posix messaging setup

--- test/posix_messaging_queue.c
+++ test/posix_messaging_queue.c
@@ -16,50 +16,56 @@ Link with -lrt.
 */
 void startprocesses(char* buff, mqd_t* mqd, struct mq_attr* attr)
 {
-	    struct message {
-                int x;
-                char y;
-                int z;
-        };
+	struct message {
+		int x;
+		char y;
+		int z;
+	};
 
-    pid_t p;
-    p = fork();
-    unsigned int priority = 0;
-    if(p<0)
-    {
-      perror("fork fail");
-      exit(1);
-    }
-    // child process because return value zero
-    else if ( p == 0){
-        struct message msg;
-        msg.x = 0;
-        msg.y = 'q';
-        msg.z = -1;
+	int relen;
+	pid_t p;
+	p = fork();
+	unsigned int priority = 0;
+	if(p == -1)
+	{
+		perror("fork fail");
+		exit(1);
+	}
+	// child process because return value zero
+	else if ( p == 0){
+		struct message msg;
+		msg.x = 0;
+		msg.y = 'q';
+		msg.z = -1;
 
-        /* Sending a struct works identically to a char array */
-        mq_send (*mqd, (const char *)&msg, sizeof (struct message), 10);
-    }
+		/* Sending a struct works identically to a char array */
+		while(1)
+			mq_send (*mqd, (const char *)&msg, sizeof (struct message), 10);
+	}
 
- 
-    // parent process because return value non-zero.
-    else{
-        printf("Hello from Parent!\n");
-	  /* When reading, use a char* buffer and explicitly cast */
-        if ((mq_receive (*mqd, buff, attr->mq_msgsize, &priority)) != -1)
-        {
-                /* Retrieve message fields here */
-		struct message* ptr = (struct messaege*) buff;
-		printf("Contents of structure %d are %c, %d\n", ptr->x, ptr->y, ptr->z);
 
-		memset( buff, '\0', attr->mq_msgsize );
-        }
-    }
+	// parent process because return value non-zero.
+	else{
+		while(1){
+			printf("Hello from Parent!\n");
+			/* When reading, use a char* buffer and explicitly cast */
+			 relen =  ((mq_receive (*mqd, buff, attr->mq_msgsize, &priority)) != -1);
+			if (relen == -1) perror("mq_receive");
+			else if (relen != sizeof(struct message)) printf ("unexpected length\n");
+			else
+			{
+				/* Retrieve message fields here */
+				struct message* ptr = (struct message*) buff;
+				printf("Contents of structure %d are %c, %d\n", ptr->x, ptr->y, ptr->z);
 
+				memset( buff, '\0', attr->mq_msgsize );
+			}
+		}
+	}
 }
 int main () {
 	/* Create and open a message queue for writing and reading*/
-	mqd_t mqd = mq_open ("/OpenCSF_MQ", O_CREAT | O_EXCL | O_NONBLOCK,  0600, NULL);
+	mqd_t mqd = mq_open ("/OpenCSF_MQ", O_CREAT | O_EXCL | O_NONBLOCK | O_RDWR,  0600, NULL);
 
 	/* Ensure the creation was successful */
 	if (mqd == -1)
@@ -68,11 +74,11 @@ int main () {
 		exit (1);
 	}
 	/* Get the message queue attributes */
-        struct mq_attr attr;
-        assert (mq_getattr (mqd, &attr) != -1);
+	struct mq_attr attr;
+	if (mq_getattr (mqd, &attr) == -1) {perror("mq_getattr error"); exit (-1);}
 
-        char *buffer = calloc (attr.mq_msgsize, 1);
-        assert (buffer != NULL);
+	char *buffer = calloc (attr.mq_msgsize, 1);
+	assert (buffer != NULL);
 
 
 	startprocesses(buffer, &mqd, &attr);
